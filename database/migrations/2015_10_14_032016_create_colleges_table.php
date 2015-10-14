42a2f0c9eead0b64d6e9504959c39570b997a1c2
JurelP <patocjurel@gmail.com>
2015-10-14 11:29:25 +0800

    added migrations

---
+++ database/migrations/2015_10_14_032016_create_colleges_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollegesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colleges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('colleges');
+    }
+}
