72e97b6e4ceb38991f400cb7600c85fb9b77017c
JurelP <patocjurel@gmail.com>
2015-10-14 14:44:14 +0800

    added view for error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="well">
+                Not Found
+            </div>
+        </div>
+    </div>
+@stop
