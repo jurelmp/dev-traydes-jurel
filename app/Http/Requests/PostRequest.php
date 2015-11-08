cfc686ac05b1b4c2fdd80335e64f1882fe8b36bf
Jurel P <patocjurel@gmail.com>
2015-11-08 21:52:51 +0800

    - added images uploader

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -26,9 +26,9 @@ class PostRequest extends Request
     public function rules()
     {
         return [
-//            'images' => 'mimes:jpeg,bmp,png',
-            /*'title' => 'required|min:5|max:255|unique:posts',
-            'content' => 'required|min:5',*/
+            'images' => 'mimes:jpeg,bmp,png',
+            'title' => 'required|min:5|max:255|unique:posts',
+            'content' => 'required|min:5',
         ];
     }
 }
